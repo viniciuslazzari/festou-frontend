aca9e1e5958f7a8e6449e87ed206be4abbf45be9
Luis Eduardo Pereira Mendes <dudupemendes13@gmail.com>
2023-08-17 11:36:29 -0300

    fix bug

--- src/pages/create_place/CreatePlace.tsx
+++ src/pages/create_place/CreatePlace.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react"
+import { useCallback, useEffect, useState, useContext } from "react"
 import Input from "../../components/input/Input"
 import "./CreatePlace.css"
 import Button from "../../components/button/Button"
