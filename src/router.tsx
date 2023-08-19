dec17cbf879bcf3164d923f524628d592b22c9d7
luiseduardomendes <dudupemendes13@gmail.com>
2023-08-19 16:47:32 -0300

    updates

--- src/router.tsx
+++ src/router.tsx
@@ -8,6 +8,7 @@ import Logout from './pages/logout/Logout';
 import CreatePlace from './pages/create_place/CreatePlace';
 import ListPlaceUser from './pages/list_places/ListPlacesUser';
 import Profile from './pages/profile/profile';
+import EditPlace from './pages/edit_place/editPlace';
 
 const Router = () => {
   return (
@@ -20,6 +21,7 @@ const Router = () => {
         <Route path='/createplace' element={<CreatePlace />} />
         <Route path='/userPlaces' element={<ListPlaceUser />} />
         <Route path='/profile' element={<Profile />} />
+        <Route path='/editPlace' element={<EditPlace />} />
       </Routes>
     </BrowserRouter>
   )
--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -5,3 +5,4 @@ export const borderColor = "#323639";
 export const redColor = "#FF2400";
 export const labelBackground = "#767676";
 export const labelInput = "rgba(255, 255, 255, 0.6)";
+export const secondarygrey = "#323639"
