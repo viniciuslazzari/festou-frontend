9edff60eb75f5a3a4b8d6a4e263fe4f9c45c9d03
Vinicius Lazzari <vinilazzari028@gmail.com>
2023-08-22 16:35:46 -0300

    fix: bug date

--- src/components/list-user-transactions-result/ListUserTransactionsResult.tsx
+++ src/components/list-user-transactions-result/ListUserTransactionsResult.tsx
@@ -50,10 +50,10 @@ const ListUserTransactionsResult = (props: ITransactionResultSection) => {
             <p> <strong> Price: </strong> R$ {item.payment}</p>
           </div>
           <div className="info-container-transaction" style={{color:labelBackground, fontSize:"16px", marginTop:"5px"}}>
-            <p> <strong> Initial date: </strong> {new Date(item.initial_date).toLocaleDateString('pt-BR')}</p>
+            <p> <strong> Initial date: </strong> {item.initial_date}</p>
           </div>
           <div className="info-container-transaction" style={{color:labelBackground, fontSize:"16px", marginTop:"5px"}}>
-            <p> <strong> Final date: </strong> {new Date(item.final_date).toLocaleDateString('pt-BR')}</p>
+            <p> <strong> Final date: </strong> {item.final_date}</p>
           </div>
         </div>
         <div className="list-transactions-button">
